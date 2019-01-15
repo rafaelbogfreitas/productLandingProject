4cf7eb128cf32cfe5ac0eb92046b7e2e5183733e
Rafael Freitas <rafaelborges1985@hotmail.com>
2019-01-15 21:38:50 +0000

    finished project

--- script.js
+++ script.js
@@ -3,10 +3,17 @@ const logo = document.querySelector("#header");
 const about = document.querySelector("#about");
 const overlay = document.querySelector(".banner-overlay");
 const overlay2 = document.querySelector(".banner-overlay2");
+const buttonSubmit = document.querySelector("#submit[value]");
 
-logo.addEventListener("click", function(e){
+//subscribe button styles
+buttonSubmit.style.fontFamily = "Dancing Script";
+buttonSubmit.style.fontSize = "0.9rem";
+buttonSubmit.style.backgroundColor = "#E7590D";
+buttonSubmit.style.color = "white";
 
-if(e.target.tagName === "IMG" && banner.style.height === "250px") {
+(logo.addEventListener("click", function(e){
+
+  if(e.target.tagName === "IMG" && banner.style.height === "250px") {
     banner.style.height = "0px";
     banner.style.transition = "all 1s"
     about.style.marginTop = "0px";
@@ -17,8 +24,9 @@ if(e.target.tagName === "IMG" && banner.style.height === "250px") {
     banner.style.height = "250px";
     overlay.style.display = "block";
     overlay2.style.display = "block";
-    about.style.marginTop = "20px";
+    //about.style.marginTop = "20px";
+    about.removeAttribute("style");
 
   }
 
-});
+}));

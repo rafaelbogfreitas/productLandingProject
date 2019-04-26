527f03f10f4e5afc45f091207683a612f1c9c38b
Rafael Freitas <rafaelborges1985@hotmail.com>
2019-04-26 14:18:09 +0100

    added logo png

--- script.js
+++ script.js
@@ -4,12 +4,55 @@ const about = document.querySelector("#about");
 const overlay = document.querySelector(".banner-overlay");
 const overlay2 = document.querySelector(".banner-overlay2");
 const buttonSubmit = document.querySelector("#submit[value]");
+const productBtn = document.querySelector("#scrollProducts");
+const aboutBtn = document.querySelector("#scrollAbout")
+const videoBtn = document.querySelector("#scrollVideo")
 
 //subscribe button styles
 buttonSubmit.style.fontFamily = "Dancing Script";
 buttonSubmit.style.fontSize = "0.9rem";
 buttonSubmit.style.backgroundColor = "#E7590D";
 buttonSubmit.style.color = "white";
+
+//nav button scrolling adjust
+productBtn.addEventListener("click", function(e){
+  if(window.innerWidth >= 999){
+    window.scrollTo({
+      left:0,
+      top:1052,
+      behavior:'smooth'});
+
+  }else {
+    window.scrollTo({
+      left:0,
+      top:1021,
+      behavior:'smooth'});
+  }
+}, false);
+
+aboutBtn.addEventListener("click", function(e){
+  if(window.innerWidth >= 999){
+    window.scrollTo({
+      left:0,
+      top:260,
+      behavior:'smooth'});
+
+  }else {
+    window.scrollTo({
+      left:0,
+      top:260,
+      behavior:'smooth'});
+  }
+}, false);
+
+videoBtn.addEventListener("click", function(e){
+  window.scrollTo({
+    left:0,
+    top:540,
+    behavior:'smooth'});
+}, false);
+
+//disapear banner when logo is clicked
 
 (logo.addEventListener("click", function(e){
 
@@ -29,4 +72,4 @@ buttonSubmit.style.color = "white";
 
   }
 
-}));
+}, false));
